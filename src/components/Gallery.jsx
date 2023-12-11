574498c8a0aa603d13b9a8f6805607da754b981e
Akash Kumar Singh <akash18011997@gmail.com>
2023-12-11 21:14:56 +0530

    day 2,3,4 photos added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,7 +11,32 @@ import pic5 from '../assets/day1/pic5.jpg'
 import pic6 from '../assets/day1/pic6.jpg'
 import pic7 from '../assets/day1/pic7.jpg'
 import pic8 from '../assets/day1/pic8.jpg'
-
+import pic21 from '../assets/day2/21.jpg'
+import pic22 from '../assets/day2/22.jpg'
+import pic23 from '../assets/day2/23.jpg'
+import pic24 from '../assets/day2/24.jpg'
+import pic25 from '../assets/day2/25.jpg'
+import pic26 from '../assets/day2/26.jpg'
+import pic27 from '../assets/day2/27.jpg'
+import pic28 from '../assets/day2/28.jpg'
+import pic29 from '../assets/day2/29.jpg'
+import pic210 from '../assets/day2/210.jpg'
+import pic31 from '../assets/day3/31.jpg'
+import pic32 from '../assets/day3/32.jpg'
+import pic33 from '../assets/day3/33.jpg'
+import pic41 from '../assets/day4/1.jpg'
+import pic42 from '../assets/day4/2.jpg'
+import pic43 from '../assets/day4/3.jpg'
+import pic46 from '../assets/day4/6.jpg'
+import pic47 from '../assets/day4/7.jpg'
+import pic48 from '../assets/day4/8.jpg'
+import pic49 from '../assets/day4/9.jpg'
+import pic410 from '../assets/day4/10.jpg'
+import pic411 from '../assets/day4/11.jpg'
+import pic412 from '../assets/day4/12.jpg'
+import pic413 from '../assets/day4/13.jpg'
+import pic414 from '../assets/day4/14.jpg'
+import pic415 from '../assets/day4/15.jpg'
 const captionStyle = {
   fontSize: '1em',
   fontWeight: 'semibold',
@@ -23,37 +48,106 @@ const slideNumberStyle = {
 const data = [
   {
     image: pic1,
-    caption: "Day 1"
   },
   {
     image: pic2,
-    caption: "Day 1"
   },
   {
     image: pic3,
-    caption: "Day 1"
   },
   {
     image: pic4,
-    caption: "Day 1"
   },
   {
     image: pic5,
-    caption: "Day 1"
   },
   {
     image: pic6,
-    caption: "Day 1"
   },
   {
     image: pic7,
-    caption: "Day 1"
   },
   {
     image: pic8,
-    caption: "Day 1"
   },
-  
+  {
+    image: pic21,
+  },
+  {
+    image: pic22,
+  },
+  {
+    image: pic23,
+  },
+  {
+    image: pic24,
+  },
+  {
+    image: pic25,
+  },
+  {
+    image: pic26,
+  },
+  {
+    image: pic27,
+  },
+  {
+    image: pic28,
+  },
+  {
+    image: pic29,
+  },
+  {
+    image: pic210,
+  },
+  {
+    image: pic31,
+  },
+  {
+    image: pic32,
+  },
+  {
+    image: pic33,
+  },
+  {
+    image: pic41,
+  },
+  {
+    image: pic42,
+  },
+  {
+    image: pic43,
+  },
+  {
+    image: pic46,
+  },
+  {
+    image: pic47,
+  },
+  {
+    image: pic48,
+  },
+  {
+    image: pic49,
+  },
+  {
+    image: pic410,
+  },
+  {
+    image: pic411,
+  },
+  {
+    image: pic412,
+  },
+  {
+    image: pic413,
+  },
+  {
+    image: pic414,
+  },
+  {
+    image: pic415,
+  },
 ];
 
 const Gallery = () => {
